c9f3b8c8434c924aaf8de283506e65ada0511285
studentmaster2020 <70553442+studentmaster2020@users.noreply.github.com>
2020-09-24 18:13:36 +0300

    Update site.js

--- site.js
+++ site.js
@@ -1,3 +1,6 @@
 require('http')
-.Server((req, res) => require('fs').createReadStream('./index.html').pipe(res))
+.Server((req, res) => {
+  res.writeHead({'Content-Type': 'text/html; charset=utf-8'});
+  require('fs').createReadStream('./index.html').pipe(res));
+} 
 .listen(process.env.PORT);
